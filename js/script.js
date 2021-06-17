640223811005a42d98132faa517a75ccfcd5f245
Roydano <roy.dano90@gmail.com>
2021-06-17 19:30:50 +0200

    add esercizio completato come da coding....da rendere più pulito

--- js/script.js
+++ js/script.js
@@ -47,15 +47,17 @@ const cats = [
 
 ];
 
+document.getElementById('milestone1').innerHTML += "MILESTONE 1";
+
 cats.forEach((elements) => {document.getElementById('milestone1').innerHTML += 
 `
     <ul>
         <li>
-            Il gatto ${elements.nome} è di colore ${elements.colore}
+            ${elements.nome} <i class="fas fa-cat" style="color:${elements.colore}"></i>
         </li>
     </ul>
 
- `
+`
 });
 
 
@@ -64,30 +66,80 @@ cats.forEach((elements) => {document.getElementById('milestone1').innerHTML +=
 
 
 
-
 // Milestone 2 Dividere i gatti in due contenitori distinti in base al sesso e aggiungere a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio. Il colore del fiocco deve essere più tenue se il gatto è più giovane, più scuro se il gatto è più vecchio.
+// const opacity = () => e;
+
+const pink = '#f2b1c2';
+const blue = '#21008d';
 
 
-const maleCats = cats.filter((elements) => elements.genere === 'maschio');
-const femaleCats = cats.filter((elements) => elements.genere === 'femmina');
+const newCats = cats.map((element) =>{
+    // const nome = element.nome;
+    // const eta = element.eta;
+    // const colore = element.colore;
+    // const genere = element.genere;
+    //! il codice ecommentato sopra possiamo abbreviarlo cosi:
+
+    const{nome, eta, colore, genere} = element
 
 
-console.log(maleCats, femaleCats);
+    const opacity = element.eta / 11;
+
+    return {
+        nome,
+        eta,
+        colore,
+        genere,
+        ribbon: {
+            colore : (genere == 'femmina') ? pink : blue,
+            opacity : opacity
+        }    
+    }
+
+});
 
 
 
 
+document.getElementById('milestone1').innerHTML += "MILESTONE 2";
 
 
+const maleCats = newCats.filter((element) => {
+    return element.genere === 'maschio'
+
+});
+
+maleCats.forEach((elements) => {
+    document.getElementById('milestone2').innerHTML += 
+`
+    <ul>
+        <li>
+            ${elements.nome} <i class="fas fa-cat" style="color:${elements.colore}"></i>
+            <i class="fas fa-ribbon" style="color:${elements.ribbon.colore}; opacity:${elements.ribbon.opacity}"></i>,
+        </li>
+    </ul>
+
+`
+});
 
 
+const femaleCats = newCats.filter((element) => {
+    return element.genere === 'femmina'
+});
 
 
+femaleCats.forEach((elements) => {
+    document.getElementById('milestone2').innerHTML += 
+`
+    <ul>
+        <li>
+            ${elements.nome} <i class="fas fa-cat" style="color:${elements.colore}"></i>
+            <i class="fas fa-ribbon" style="color:${elements.ribbon.colore}; opacity:${elements.ribbon.opacity}"></i>,
+        </li>
+    </ul>
 
-
-
-
-
+`
+});
 
 
 
@@ -95,3 +147,34 @@ console.log(maleCats, femaleCats);
 
 
 // Milestone 3 Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini maschio, inserendo solamente nome, colore e opacità del fiocco per ogni gatto.
+
+
+let catsMaleFemale = [...femaleCats, ...maleCats];
+
+const catsFemaleMaleZippato = catsMaleFemale.map((element) =>{
+
+    const {nome, colore, ribbon} = element;
+
+    return{
+        nome,
+        colore,
+        ribbon
+    }
+
+});
+
+document.getElementById('milestone3').innerHTML += "MILESTONE 3";
+
+catsFemaleMaleZippato.forEach((elements) => {
+
+    document.getElementById('milestone3').innerHTML += 
+`
+    <ul>
+        <li>
+            ${elements.nome} <i class="fas fa-cat" style="color:${elements.colore}"></i>
+            <i class="fas fa-ribbon" style="color:${elements.ribbon.colore}; opacity:${elements.ribbon.opacity}"></i>,
+        </li>
+    </ul>
+
+`
+});
